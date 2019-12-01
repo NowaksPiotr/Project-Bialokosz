0f60c7b9b6cacb062d228b3bf1630c9e986ad47f
NowaksPiotr <nowak.spiotr@gmail.com>
2019-12-01 18:53:07 +0100

    Commit

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let hamburger = document.querySelector('.hamburger');
 let hamburgerBar = document.querySelector('.hamburger__bar');
 let nav = document.querySelector('.nav');
-let welcomeMobileLogo = document.getElementById("mobileLogo");
 
 hamburger.addEventListener('click', openHamburgerMenu);
 
@@ -9,8 +8,3 @@ function openHamburgerMenu() {
     hamburgerBar.classList.toggle('hamburger__bar--active');
     nav.classList.toggle('nav--active');
 }
-
-setInterval(function welcome(){
-    welcomeMobileLogo.classList.remove('logo__hidden');
-    welcomeMobileLogo.classList.add('index-logo__bialokosz__mobile');
-}, 800)
