1834bddb1ce1ecc4b96319d1f904c79538c175f3
NowaksPiotr <nowak.spiotr@gmail.com>
2019-12-01 18:11:00 +0100

    Added welcome photo.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let hamburger = document.querySelector('.hamburger');
 let hamburgerBar = document.querySelector('.hamburger__bar');
 let nav = document.querySelector('.nav');
+let welcomeMobileLogo = document.getElementById("mobileLogo");
 
 hamburger.addEventListener('click', openHamburgerMenu);
 
@@ -8,3 +9,8 @@ function openHamburgerMenu() {
     hamburgerBar.classList.toggle('hamburger__bar--active');
     nav.classList.toggle('nav--active');
 }
+
+setInterval(function welcome(){
+    welcomeMobileLogo.classList.remove('logo__hidden');
+    welcomeMobileLogo.classList.add('index-logo__bialokosz__mobile');
+}, 800)
