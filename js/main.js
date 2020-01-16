00620ffa2ce853a4aadb426288372c018b352e6c
NowaksPiotr <nowak.spiotr@gmail.com>
2020-01-16 16:11:02 +0100

    Changed structure of navigation block in index.html. Added clickable bacground to close hamburger menu.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-let hamburger = document.querySelector('.hamburger');
+let hamburger = document.querySelector('.hamburger__wrapper');
 let hamburgerBar = document.querySelector('.hamburger__bar');
-let nav = document.querySelector('.nav');
+let nav = document.querySelector('.navigation__menu');
+const closeHamburgerBackground = document.querySelector('.background');
 
 hamburger.addEventListener('click', openHamburgerMenu);
+closeHamburgerBackground.addEventListener('click', hamburgerClose);
 
 function openHamburgerMenu() {
     hamburgerBar.classList.toggle('hamburger__bar--active');
-    nav.classList.toggle('nav--active');
+    nav.classList.toggle('navigation__menu--active');
+    closeHamburgerBackground.classList.toggle('background--active');
+}
+
+function hamburgerClose() {
+    hamburgerBar.classList.remove('hamburger__bar--active');
+    nav.classList.remove('navigation__menu--active');
+    closeHamburgerBackground.classList.remove('background--active');
 }
