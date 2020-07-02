898f2bc4c3d9aea5583bd5e2f03435f8c9f5806e
Szamponetti <nowak.spiotr@gmail.com>
2020-07-02 12:57:40 +0200

    New idea for galleries.

--- js/sliders.js
+++ js/sliders.js
@@ -5,41 +5,42 @@ const buttonNext = document.querySelectorAll(".gallery__button-next");
 
 // Variables
 let counter = 0;
-let photosArray = [
-  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-];
-let pp = ["odbyt", "jaja"];
 let noclegi = document.querySelector(".noclegi");
 // Interval
 let intervalID = setInterval(changePhoto, 8000);
 let flag = true;
 
+//Photos
+let allPhotos = [{
+  siteName: 'index',
+  folderName: [1,2,3],
+  slider: [[1,2,3,4,5,6,7,8,9,10],[1,2,3,4],[1,2,3]],
+}];  
 /*********************************************************/
-
+console.log(allPhotos[0].slider[1].length); 
 buttonNext.forEach(function (nextButton, index) {
   nextButton.addEventListener("click", nextPhoto);
   function nextPhoto() {
     let photo = document.querySelectorAll(".slide__photos-bialokosz");
     let newPhoto = document.createElement("img");
     counter++;
-    if (counter == 10) {
+    if(counter > allPhotos[0].slider[index].length-1){
       counter = 0;
     }
     photo[index].remove();
     newPhoto.setAttribute("class", "slide__photos-bialokosz");
-    if(noclegi){
     newPhoto.setAttribute(
       "src",
-      "./assets/photos/slider/noclegi/" + photosArray[0][counter] + ".jpg"
+      "./assets/photos/slider/index/" + allPhotos[0].folderName[index] + '/' + allPhotos[0].slider[index][counter] + ".jpg"
     );
-    }
+    console.log(newPhoto);
     photoWrapper[index].append(newPhoto);
     clearInterval(intervalID);
     flag = false;
     checkInterval();
+    }
   }
-});
+);
 buttonPrevious.forEach(function (previousButton, index) {
   previousButton.addEventListener("click", previousPhoto);
   function previousPhoto() {
