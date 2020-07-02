88a99e28187518bac4dcb9115a5d8f6ad43d2451
Szamponetti <nowak.spiotr@gmail.com>
2020-07-02 15:17:48 +0200

    Working on sliders.

--- js/sliders.js
+++ js/sliders.js
@@ -5,64 +5,172 @@ const buttonNext = document.querySelectorAll(".gallery__button-next");
 
 // Variables
 let counter = 0;
-let noclegi = document.querySelector(".noclegi");
+//WHICH SITE RECOGNITION
+let documentTitle = document.title;
 // Interval
-let intervalID = setInterval(changePhoto, 8000);
+let intervalID = setInterval(changePhoto, 4000);
 let flag = true;
 
 //Photos
-let allPhotos = [{
-  siteName: 'index',
-  folderName: [1,2,3],
-  slider: [[1,2,3,4,5,6,7,8,9,10],[1,2,3,4],[1,2,3]],
-}];  
+let allPhotos = [
+  {
+    siteName: "Pałac Białokosz",
+    folderName: [1, 2, 3],
+    slider: [
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      [1, 2, 3, 4],
+      [1, 2, 3],
+    ],
+  },
+  {
+    siteName: "Noclegi Pałac Białokosz",
+    folderName: [1, 2, 3, 4, 5],
+    slider: [
+      [1, 2, 3, 4],
+      [1, 2, 3, 4],
+      [1, 2, 3, 4],
+      [1, 2, 3, 4],
+      [1, 2, 3, 4],
+    ],
+  },
+  {
+    siteName: "Restauracja Pałac Białokosz",
+    folderName: [1,2,3],
+    slider: [
+      [1,2,3,4,5,6,7,8,9,10],
+      [1,2,3,4,5],
+      [1,2,3]
+    ]
+  },
+  {
+    siteName: "Wesele Pałac Białokosz",
+    folderName: [1],
+    slider: [
+      [1,2,3,4,5,6,7,8,9,10,11,12,13]
+    ]
+  }
+];
 /*********************************************************/
-console.log(allPhotos[0].slider[1].length); 
 buttonNext.forEach(function (nextButton, index) {
   nextButton.addEventListener("click", nextPhoto);
   function nextPhoto() {
     let photo = document.querySelectorAll(".slide__photos-bialokosz");
     let newPhoto = document.createElement("img");
     counter++;
-    if(counter > allPhotos[0].slider[index].length-1){
-      counter = 0;
-    }
     photo[index].remove();
     newPhoto.setAttribute("class", "slide__photos-bialokosz");
-    newPhoto.setAttribute(
-      "src",
-      "./assets/photos/slider/index/" + allPhotos[0].folderName[index] + '/' + allPhotos[0].slider[index][counter] + ".jpg"
-    );
-    console.log(newPhoto);
+    if (allPhotos[0].siteName === documentTitle) {
+      if (counter > allPhotos[0].slider[index].length - 1) {
+        counter = 0;
+      }
+      newPhoto.setAttribute(
+        "src",
+        "./assets/photos/slider/index/" +
+          allPhotos[0].folderName[index] +
+          "/" +
+          allPhotos[0].slider[index][counter] +
+          ".jpg"
+      );
+    } else if (allPhotos[1].siteName === documentTitle) {
+      if (counter > allPhotos[1].slider[index].length - 1) {
+        counter = 0;
+      }
+      newPhoto.setAttribute(
+        "src",
+        "./assets/photos/slider/noclegi/" +
+          allPhotos[1].folderName[index] +
+          "/" +
+          allPhotos[1].slider[index][counter] +
+          ".jpg"
+      );
+    }else if (allPhotos[2].siteName === documentTitle) {
+      if (counter > allPhotos[2].slider[index].length - 1) {
+        counter = 0;
+      }
+      newPhoto.setAttribute(
+        "src",
+        "./assets/photos/slider/restauracja/" +
+          allPhotos[2].folderName[index] +
+          "/" +
+          allPhotos[2].slider[index][counter] +
+          ".jpg"
+      );
+    }else if(allPhotos[3].siteName === documentTitle) {
+      if (counter > allPhotos[3].slider[index].length - 1) {
+        counter = 0;
+      }
+      newPhoto.setAttribute(
+        "src",
+        "./assets/photos/slider/wesele/" +
+          allPhotos[3].folderName[index] +
+          "/" +
+          allPhotos[3].slider[index][counter] +
+          ".jpg"
+      );
+    }
     photoWrapper[index].append(newPhoto);
     clearInterval(intervalID);
     flag = false;
     checkInterval();
-    }
   }
-);
+});
 buttonPrevious.forEach(function (previousButton, index) {
   previousButton.addEventListener("click", previousPhoto);
   function previousPhoto() {
     let photo = document.querySelectorAll(".slide__photos-bialokosz");
     let newPhoto = document.createElement("img");
     counter--;
-    if (counter < 0) {
-      counter = 9;
-    }
     photo[index].remove();
     newPhoto.setAttribute("class", "slide__photos-bialokosz");
-    if(noclegi){
+    if (allPhotos[0].siteName === documentTitle) {
+      if (counter < 0) {
+        counter = allPhotos[0].slider[index].length - 1;
+      }
       newPhoto.setAttribute(
         "src",
-        "./assets/photos/slider/noclegi/" + photosArray[1][counter] + ".jpg"
+        "./assets/photos/slider/index/" +
+          allPhotos[0].folderName[index] +
+          "/" +
+          allPhotos[0].slider[index][counter] +
+          ".jpg"
       );
-    }else{
-      newPhoto.setAttribute(
-      "src",
-      "./assets/photos/slider/" + photosArray[0][counter] + ".jpg"
-    );
+    } else if (allPhotos[1].siteName === documentTitle) {
+      if (counter < 0) {
+        counter = allPhotos[1].slider[index].length - 1;
       }
+      newPhoto.setAttribute(
+        "src",
+        "./assets/photos/slider/noclegi/" +
+          allPhotos[1].folderName[index] +
+          "/" +
+          allPhotos[1].slider[index][counter] +
+          ".jpg"
+      );
+    }else if (allPhotos[2].siteName === documentTitle) {
+      if (counter < 0) {
+        counter = allPhotos[2].slider[index].length - 1;
+      }
+      newPhoto.setAttribute(
+        "src",
+        "./assets/photos/slider/restauracja/" +
+          allPhotos[2].folderName[index] +
+          "/" +
+          allPhotos[2].slider[index][counter] +
+          ".jpg"
+      );
+    }else if(allPhotos[3].siteName === documentTitle) {
+      if (counter < 0) {
+        counter = allPhotos[3].slider[index].length - 1;
+      }
+      newPhoto.setAttribute(
+        "src",
+        "./assets/photos/slider/wesele/" +
+          allPhotos[3].folderName[index] +
+          "/" +
+          allPhotos[3].slider[index][counter] +
+          ".jpg"
+      );
+    }
     photoWrapper[index].append(newPhoto);
     clearInterval(intervalID);
     flag = false;
@@ -73,28 +181,70 @@ buttonPrevious.forEach(function (previousButton, index) {
 function checkInterval() {
   if (flag != true) {
     flag = true;
-    intervalID = setInterval(changePhoto, 8000);
+    intervalID = setInterval(changePhoto, 4000);
   }
 }
 function changePhoto() {
   counter++;
-  if (counter == 10) {
-    counter= 0;
-  }
-  photoWrapper.forEach(function changing(photosWrapper, index){
-      let photo = document.querySelectorAll(".slide__photos-bialokosz");
-      let newPhoto = document.createElement("img");
-      photo[index].remove();
-      newPhoto.setAttribute("class", "slide__photos-bialokosz");
-      if(noclegi){
-      newPhoto.setAttribute(
-        "src",
-        "./assets/photos/slider/noclegi/" + photosArray[1][counter] + ".jpg");
-      }else{
-      newPhoto.setAttribute(
-        "src",
-        "./assets/photos/slider/" + photosArray[0][counter] + ".jpg"
-      );
+  photoWrapper.forEach(function changing(photosWrapper, index) {
+    if (allPhotos[0].siteName === documentTitle) {
+      if (counter > allPhotos[0].slider[index].length - 1) {
+        counter = 0;
       }
-      photosWrapper.append(newPhoto);
-    })}
+    } else if (allPhotos[1].siteName === documentTitle) {
+      if (counter > allPhotos[1].slider[index].length - 1) {
+        counter = 0;
+      }
+    }else if(allPhotos[2].siteName === documentTitle) {
+      if (counter > allPhotos[2].slider[index].length - 1) {
+        counter = 0;
+      }
+    }else if(allPhotos[3].siteName === documentTitle) {
+      if (counter > allPhotos[3].slider[index].length - 1) {
+        counter = 0;
+      }
+    }
+    let photo = document.querySelectorAll(".slide__photos-bialokosz");
+    let newPhoto = document.createElement("img");
+    photo[index].remove();
+    newPhoto.setAttribute("class", "slide__photos-bialokosz");
+    if(allPhotos[0].siteName === documentTitle){
+    newPhoto.setAttribute(
+      "src",
+      "./assets/photos/slider/index/" +
+        allPhotos[0].folderName[index] +
+        "/" +
+        allPhotos[0].slider[index][counter] +
+        ".jpg"
+    );
+    }else if(allPhotos[1].siteName === documentTitle){
+      newPhoto.setAttribute(
+        "src",
+        "./assets/photos/slider/noclegi/" +
+          allPhotos[1].folderName[index] +
+          "/" +
+          allPhotos[1].slider[index][counter] +
+          ".jpg"
+      );
+    }else if(allPhotos[2].siteName === documentTitle){
+      newPhoto.setAttribute(
+        "src",
+        "./assets/photos/slider/restauracja/" +
+          allPhotos[2].folderName[index] +
+          "/" +
+          allPhotos[2].slider[index][counter] +
+          ".jpg"
+      );
+    }else if(allPhotos[3].siteName === documentTitle){
+      newPhoto.setAttribute(
+        "src",
+        "./assets/photos/slider/wesele/" +
+          allPhotos[3].folderName[index] +
+          "/" +
+          allPhotos[3].slider[index][counter] +
+          ".jpg"
+      );
+    }
+    photosWrapper.append(newPhoto);
+  });
+}
